e9187a78ef7b9438f8afa20e0ef21291b9dcab51
umashankar mahto <shyammht3@gmail.com>
2025-10-01 15:45:15 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Highlights from './components/Highlights'
 import Model from './components/Model'
 import Features from './components/Features'
 import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
 
 const App = () => {
 
@@ -16,6 +17,7 @@ const App = () => {
       <Model/>
       <Features/>
       <HowItWorks/>
+      <Footer/>
     </main>
   )
 }
